07c496bc3cef72d7d6c81a348f67b0b5fec4ce49
NISHANT KUMAR GUPTA (IN) <nishant.gupta1000013@homecredit.co.in>
2019-12-08 20:13:46 +0530

    Eureka Server Creation Code Commit

---
+++ src/main/java/com/eurekaserver/eureka/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.eurekaserver.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
